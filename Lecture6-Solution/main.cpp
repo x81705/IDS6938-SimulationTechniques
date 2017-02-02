452e932b675295f67c750c9a9f9fad6c0807774f
Joe Kider <jkider@ist.ucf.edu>
2017-02-02 09:06:21 -0500

    Solution for Lecture 6 - RK2 and RK4 methods.

---
+++ Lecture6-Solution/main.cpp
@@ -0,0 +1,123 @@
+//Eulers Method to solve a differential equation
+#define _USE_MATH_DEFINES
+
+#include <cmath>
+#include<iostream>
+#include<iomanip>
+#include<math.h>
+#include <string>
+#include <sstream>
+
+/*
+Compute the dy/dx
+*/
+double df(double x, double y)            //function for defining dy/dx
+{
+    return (y * log(y)) / x;
+}
+
+
+/*
+Compute the exact answer
+*/
+double exact(double x)            //function for defining dy/dx
+{
+    return pow(M_E, (x / 2));
+}
+
+
+/*
+Calculate the error
+*/
+double error(double exact, double approx)
+{
+    return abs(exact - approx) / exact;
+}
+
+
+/*
+This implements the Euler method equation
+*/
+double rk1(double y, double h, double x)
+{
+    return y + h * df(x, y);
+}
+
+
+/*
+This implements the Midpoint method equation
+*/
+double rk2(double y, double h, double x)
+{
+	double k1 = df(x, y);
+	double h_half = h / 2.0;
+    return  y + h * df( x + h_half, y + (h_half * k1) );
+}
+
+
+/*
+This implements the RK4 method equation
+*/
+double rk4(double y, double h, double x)
+{
+	double h_half = h/2;
+	double k1 = df(x, y);
+	double k2 = df(x+ h_half, y + (h_half * k1));
+	double k3 = df(x + h_half, y + (h_half * k2));;
+	double k4 = df(x + h, y + (h * k3));;
+    return  y + h * (1.0 / 6.0)*(k1 + 2 * k2 + 2 * k3 + k4);
+}
+
+
+
+std::string toString(double x, double y_euler, double y_midpoint, double y_RK4, double y_exact, double presision)
+{
+	
+	std::ostringstream out;
+	out << std::setprecision(2) << x << "\t";
+	out << std::setprecision(presision) << y_euler << "\t";
+	out << std::setprecision(presision) << y_midpoint << "\t";
+	out << std::setprecision(presision) << y_RK4 << "\t";
+	out << std::setprecision(presision) << y_exact << "\t";
+	out << std::setprecision(5) << error(exact(x), y_euler) << "%\t";
+	out << std::setprecision(5) << error(exact(x), y_midpoint) << "%\t";
+	out << std::setprecision(5) << error(exact(x), y_RK4)<<"%";
+	
+	return   out.str();
+}
+
+
+
+
+int main()
+{
+
+    double y_euler = M_E;
+	double y_midpoint = M_E;
+	double y_RK4 = M_E;
+    double x = 2.0;
+    double h = 0.1;
+
+	std::cout << std::fixed << std::showpoint;
+    // Header information for column printouts
+	std::cout << "x" << "\t" << "y (Euler)" << "\t" << "y (midpoint)" << "\t" << "y (RK44)" << "\t"
+		<< "EXACT" << "\t\t" << "%Err(E)" << "\t\t" << "%Err(M)" << "\t\t" << "%Err(RK4)" << std::endl;
+	std::cout << "----" << "\t" << "----------" << "\t" << "----------" << "\t" << "----------"
+		<< "\t" << "----------" << "\t" << "-------"<<"\t\t" << "-------" << "\t\t" << "------" << std::endl;
+
+	//intial values
+	std::cout<< toString(x, y_euler, y_midpoint, y_RK4, exact(x), 10) << std::endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+		y_euler = rk1(y_euler,h,x);  //caculate y_{i+1}
+		y_midpoint = rk2(y_midpoint, h, x);  //caculate y_{i+1}
+		y_RK4 = rk4(y_RK4, h, x);  //caculate y_{i+1}
+        x = x + h;       //increment x
+
+		std::cout << toString(x, y_euler, y_midpoint, y_RK4, exact(x), 10) << std::endl;
+	}
+
+
+    return 0;
+}
