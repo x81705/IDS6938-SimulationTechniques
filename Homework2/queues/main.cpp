f827b38538b870f55fe86ccf255d69719bab63af
charles.timm <charles.r.timm@knights.ucf.edu>
2017-03-27 17:14:47 -0400

    Queues - help from William Netterville with code

--- Homework2/queues/main.cpp
+++ Homework2/queues/main.cpp
@@ -24,6 +24,7 @@ void pause()
 }
 
 //add extra line from MM1 Queue Objects for Part III?
+//MYOBJECTNAME.autogenerate_new_arrivals(false); //Added
 
 int main(int argc, char* argv[])
 {
@@ -34,39 +35,60 @@ int main(int argc, char* argv[])
    {
       
 	   //TODO Create MM1_Queue objects to capture the airport senario.
+	   MM1_Queue    Q1;
+	   Q1.set_file_names("Q1_log.txt", "Q1_wait.txt", "Q1_service.txt");
+	   Q1.set_lambda(6);   // for this assignment this is set to a variable from the for loop.
+	   Q1.set_mu(53);
+	   Q1.initialize();
+	   Q1.set_seed(1, rd());   // I set the first one to 1 for testing, the others you should use two random seeds (rd(), rd())
 
-	   /*MM1_Queue MYOBJECTNAME;
-	   MYOBJECTNAME.set_file_names("01_log.txt", "01_wait.txt", 01_service.txt");
-	   MYOBJECTNAME.set_lambda(6);
-	   MYOBJECTNAME.set_mu(45);
-	   MYOBJECTNAME.autogenerate_new_arrivals(false);
-	   MYOBJECTNAME.is_within_confidence_interval();
-	   MYOBJECTNAME.initialize();
-	   MYOBJECTNAME.set_seed(1, rd());
-	   !MYOBECTNAME.is_within_error_range(0.006)||
-	   !MYOBECTNAME1.is_within_error_range(0.006)||
-	   !MYOBECTNAME2.is_within_error_range(0.006)||
-	   !MYOBECTNAME3.is_within_error_range(0.006)
-	   Customer cust = MYOBJECTNAME.process_next_event();
-	   MYOBJECTNAME.add_external_arrival();
-	   MYOBJECTNAE.get_current_time();
-	   MYOBJECTNAME-plot_results_output();
-	   MYOBJECTNAME.output(); cout << ******** << end1;
-	   MYOBJECTNAME_0.output(); cout << ******** << end1;
-	   MYOBJECTNAME_1.output(); cout << ******** << end1;
-	   MYOBJECTNAME_2.output(); cout << ******** << end1;
-	   MYOBJECTNAME_3.output(); cout << ******** << end1;
-	   */
+	   MM1_Queue    Q2;
+	   Q2.set_file_names("Q2_log.txt", "Q2_wait.txt", "Q2_service.txt");
+	   Q2.set_lambda(6);   // for this assignment this is set to a variable from the for loop.
+	   Q2.set_mu(20);
+	   Q2.autogenerate_new_arrivals(false);
+	   Q2.initialize();
+	   Q2.set_seed(1, rd());   // I set the first one to 1 for testing, the others you should use two random seeds (rd(), rd())
+
+	   MM1_Queue    Q3;
+	   Q3.set_file_names("Q3_log.txt", "Q3_wait.txt", "Q3_service.txt");
+	   Q3.set_lambda(6);   // for this assignment this is set to a variable from the for loop.
+	   Q3.set_mu(20);
+	   Q3.autogenerate_new_arrivals(false);
+	   Q3.initialize();
+	   Q3.set_seed(1, rd());   // I set the first one to 1 for testing, the others you should use two random seeds (rd(), rd())
+
+	   MM1_Queue    Q4;
+	   Q4.set_file_names("Q4_log.txt", "Q4_wait.txt", "Q4_service.txt");
+	   Q4.set_lambda(6);   // for this assignment this is set to a variable from the for loop.
+	   Q4.set_mu(20);
+	   Q4.autogenerate_new_arrivals(false);
+	   Q4.initialize();
+	   Q4.set_seed(1, rd());   // I set the first one to 1 for testing, the others you should use two random seeds (rd(), rd())
+
+	   MM1_Queue    Q5;
+	   Q5.set_file_names("Q5_log.txt", "Q5_wait.txt", "Q5_service.txt");
+	   Q5.set_lambda(6);   // for this assignment this is set to a variable from the for loop.
+	   Q5.set_mu(80);
+	   Q5.initialize();
+	   Q5.set_seed(1, rd());   // I set the first one to 1 for testing, the others you should use two random seeds (rd(), rd())
 
    for (; 
 		//TODO: add is_within_error_range check
+	   !Q1.is_within_error_range(0.002) ||
+	   !Q2.is_within_error_range(0.002) ||
+	   !Q3.is_within_error_range(0.002) ||
+	   !Q4.is_within_error_range(0.002) ||
+	   !Q5.is_within_error_range(0.002) 
        ;)
    {
-	   Customer cust  ;    // =  TODO: process next event;
-	   Customer cust2  ;   // =  TODO: process next event;
-	   Customer cust3  ;   // =  TODO: process next event;
-	   Customer cust4  ;   // =  TODO: process next event;
+	   Customer cust = Q1.process_next_event();     // =  TODO: process next event;
+	   Customer cust2 = Q2.process_next_event();    // =  TODO: process next event;
+	   Customer cust3 = Q3.process_next_event();    // =  TODO: process next event;
+	   Customer cust4 = Q4.process_next_event();    // =  TODO: process next event;
+	   Customer cust5 = Q5.process_next_event();    // =  TODO: process next event;
 	   //TODO: one more process_next_event for the last object.
+
 
        if (cust.get_type() == Customer::COMPLETED())
        {
@@ -74,12 +96,15 @@ int main(int argc, char* argv[])
           {
             case 0:
 				//TODO add_external_arrival() for your security gates;
+				Q2.add_external_arrival();
                  break;
             case 1:
 				//TODO add_external_arrival() for your security gates;
+				Q3.add_external_arrival();
                  break;
             case 2:
                 //TODO add_external_arrival() for your security gates;
+				Q4.add_external_arrival();
                  break;
           }
           next++;
@@ -88,6 +113,7 @@ int main(int argc, char* argv[])
        if (cust2.get_type() == Customer::COMPLETED() || cust3.get_type() == Customer::COMPLETED() || cust4.get_type() == Customer::COMPLETED())
        {
 		   //TODO add_external_arrival(); on your final boarding MM1_Queue object
+		   Q5.add_external_arrival();
        }
    }
 
@@ -98,8 +124,14 @@ int main(int argc, char* argv[])
 
 
    //**************************************************************************
+   //MYOBJECTNAME.get_current_time()
+   //MYOBJECTNAME.plot_results_output();
 
-
+   Q1.output(); cout << "*********" << endl;
+   Q2.output(); cout << "*********" << endl;
+   Q3.output(); cout << "*********" << endl;
+   Q4.output(); cout << "*********" << endl;
+   Q5.output(); cout << "*********" << endl;
 
    }
 
--- Homework2/queues/mm1_queue.cpp
+++ Homework2/queues/mm1_queue.cpp
@@ -76,12 +76,12 @@ void MM1_Queue::initialize()
     {
 
       //TODO---------------------------------------------
-      expected_server_utilization_ ;
-      expected_idle_prob_ ;
-      expected_queue_length_ ;
-      expected_number_customers_ ;
-      expected_waiting_time_ ;
-      expected_response_time_ ;
+		expected_server_utilization_ = lambda_ / mu_;
+		expected_idle_prob_ = 1 - expected_server_utilization_;
+		expected_queue_length_ = expected_server_utilization_*expected_server_utilization_ / (expected_idle_prob_);
+		expected_number_customers_ = expected_server_utilization_ / (expected_idle_prob_);
+		expected_waiting_time_ = expected_server_utilization_ / (mu_ - lambda_);
+		expected_response_time_ = 1 / (mu_ - lambda_);
       // ------------------------------------------------
 
       rnd_arrival.set_rate(lambda_);
